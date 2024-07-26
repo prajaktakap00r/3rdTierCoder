056038d67f975b536e304dc0fc3b8cd04c7a9b23
prajakta <89696648+prajaktakap00r@users.noreply.github.com>
2024-07-26 21:37:44 +0530

    margins

--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -1,7 +1,7 @@
 import logo from "../assets/leetcodeLogo.png";
 export default function Navbar() {
   return (
-    <header className="p-4 flex justify-between items-center bg-white h-[4rem] overflow-hidden">
+    <header className="p-4 px-20 flex justify-between items-center bg-white h-100 overflow-hidden">
       <div className="text-2xl">
         <img src={logo} className="w-10 h-10" />
       </div>
